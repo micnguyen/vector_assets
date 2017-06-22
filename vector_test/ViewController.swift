64cb0a309b2f5abd1282816f4307b566e24e5ff4
Michael Nguyen <nguyen.michael0@gmail.com>
2017-06-22 12:10:48 +1000

    Initial Commit

---
+++ vector_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vector_test
+//
+//  Created by Michael Nguyen on 22/6/17.
+//  Copyright © 2017 Michael Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
